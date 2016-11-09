0e0e3bfac72e7e150ec4997fbeb01495e2abb8cf
David <david@hotdave>
2016-11-09 20:36:17 +0100

    Last stuff

--- src/player/agents/Dolores.java
+++ src/player/agents/Dolores.java
@@ -13,13 +13,11 @@ import java.util.LinkedList;
  */
 public class Dolores extends Agent {
 
-    private boolean timeIsUp = false;
-    private final int INFINITY = 100000;
-    private final int NEGATIVE_INFINITY = -10000;
+    private final float INFINITY = 100000;
+    private final float NEGATIVE_INFINITY = -10000;
     private final float TIME_LIMIT = 2000;
 
     private Color myColor;
-    private Color opponentColor;
     private long startTime;
 
     public Dolores(Color color) {
@@ -42,17 +40,17 @@ public class Dolores extends Agent {
         startTimer();
 
         while (proceed) {
-            int bestScore = NEGATIVE_INFINITY;
+            float bestScore = NEGATIVE_INFINITY;
             Position currentBestPosition = null;
 
             for (Position position : currentLegalPositions) {
-                int currentScore;
+                float currentScore;
                 GameBoard childBoard = new GameBoard(board.copyMatrix());
                 childBoard.placeDisk(myColor, position);
 
                 currentScore = minimax(childBoard, depth, NEGATIVE_INFINITY, INFINITY, false);
 
-                if (timeIsUp) {
+                if (isTimeUp()) {
                     proceed = false;
                     break;
                 }
@@ -70,28 +68,27 @@ public class Dolores extends Agent {
             }
 
         }
-        return bestPosition;
+        return new Position(bestPosition);
     }
 
 
-    public int minimax(GameBoard board, int depth, int alpha, int beta, boolean maxPlayer) {
+    public float minimax(GameBoard board, int depth, float alpha, float beta, boolean maxPlayer) {
         LinkedList<Position> legalPosition = board.getAllLegalPositions(myColor);
-        int score = 0;
+        float score = 0;
 
         if(depth == 0 || legalPosition.isEmpty()) {
             return costFunction(board);
         }
 
-        if ((timeIsUp = isTimeUp())) {
+        if (isTimeUp()) {
             return score;
         }
 
-        if (maxPlayer || !timeIsUp) {
+        if (maxPlayer && !isTimeUp()) {
             score = NEGATIVE_INFINITY;
             for (Position position : legalPosition) {
                 GameBoard childBoard = new GameBoard(board.copyMatrix());
                 childBoard.placeDisk(myColor, position);
-
 
                 score = max(score, minimax(childBoard, depth - 1, alpha, beta, !maxPlayer));
                 alpha = max(alpha, score);
@@ -100,29 +97,31 @@ public class Dolores extends Agent {
                     break;
                 }
             }
-        } else if(!timeIsUp) {
+            return score;
+        } else if(!isTimeUp()) {
             score = INFINITY;
             for (Position position : legalPosition) {
                 GameBoard childBoard = new GameBoard(board.copyMatrix());
                 childBoard.placeDisk(Color.opposite(myColor), position);
 
-                score = max(score, minimax(childBoard, depth - 1, alpha, beta, !maxPlayer));
+                score = min(score, minimax(childBoard, depth - 1, alpha, beta, !maxPlayer));
                 beta = min(beta, score);
 
                 if (beta <= alpha) {
                     break;
                 }
             }
+            return score;
         }
 
         return score;
     }
 
-    private int max(int first, int second) {
+    private float max(float first, float second) {
         return first > second ? first : second;
     }
 
-    private int min(int first, int second) {
+    private float min(float first, float second) {
         return first < second ? first : second;
     }
 
@@ -131,33 +130,90 @@ public class Dolores extends Agent {
     }
 
     private boolean isTimeUp() {
-        if ((System.currentTimeMillis() - startTime) > (TIME_LIMIT - 500)) {
+        if ((System.currentTimeMillis() - startTime) > (TIME_LIMIT - 10)) {
             return true;
         }
 
         return false;
     }
 
-    private int costFunction(GameBoard board) {
-        int nMyDisks = board.getNumberOfDisksInColor(myColor);
-        int nMoves = board.getAllLegalPositions(myColor).size();
-        int nCorners = countCorners(board);
+    private float costFunction(GameBoard board) {
+        int nWMyDisks = board.getNumberOfDisksInColor(myColor);
+        int nWMoves = board.getAllLegalPositions(myColor).size();
+        int nWCorners = countCorners(board, myColor);
+        int nWEdgeDisks = countEdges(board, myColor);
 
-        return 5*nMyDisks + 10*nMoves + 500*nCorners;
+        int nBMyDisks = board.getNumberOfDisksInColor(Color.opposite(myColor));
+        int nBMoves = board.getAllLegalPositions(Color.opposite(myColor)).size();
+        int nBCorners = countCorners(board, Color.opposite(myColor));
+        int nBEdgeDisks = countEdges(board, Color.opposite(myColor));
+
+        float score = 5*scoreParity(nWMyDisks, nBMyDisks) + 10*scoreParity(nWMoves, nBMoves) +
+                50*scoreParity(nWCorners, nBCorners) + 15*scoreParity(nWEdgeDisks, nBEdgeDisks);
+
+        System.out.println(score);
+
+        return score;
     }
 
-    private int countCorners(GameBoard board) {
+    private int countCorners(GameBoard board, Color color) {
         int nCorners = 0;
         Color[][] boardMatrix = board.getBoardMatrix();
 
         for (int x = 0; x < 8; x+=7) {
             for (int y = 0; y < 8; y+=7) {
-                if (boardMatrix[x][y] == myColor) {
+                if (boardMatrix[x][y] == color) {
                     nCorners++;
                 }
             }
         }
 
         return nCorners;
+    }
+
+    private int countEdges(GameBoard board, Color color) {
+        int y;
+        int x;
+        int edgeDisks = 0;
+        Color[][] boardMatrix = board.getBoardMatrix();
+
+        y = 0;
+        for(x=0; x<8; x++){
+            if (boardMatrix[x][y] == color) {
+                edgeDisks++;
+            }
+        }
+
+        y = 7;
+        for(x=0; x<8; x++){
+            if (boardMatrix[x][y] == color) {
+                edgeDisks++;
+            }
+        }
+
+        x=0;
+        for(y=1; y<7; y++){
+            if (boardMatrix[x][y] == color) {
+                edgeDisks++;
+            }
+        }
+
+        x=7;
+        for(y=1; y<7; y++){
+            if (boardMatrix[x][y] == color) {
+                edgeDisks++;
+            }
+        }
+
+        return edgeDisks;
+    }
+
+    public float scoreParity(int maxScore, int minScore) {
+        if((maxScore + minScore) > 0)
+        {
+            return 10*((float)(maxScore - minScore)/(float)(maxScore + minScore));
+        } else {
+            return 0;
+        }
     }
 }
