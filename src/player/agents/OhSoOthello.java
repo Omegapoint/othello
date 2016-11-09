a5f8d9f8bcfaef7d76b16f3ec1775afe1e231d0c
xist <xist@raidlineup.com>
2016-11-09 20:26:33 +0100

    OhSoOthello now likes corners

--- src/player/agents/OhSoOthello.java
+++ src/player/agents/OhSoOthello.java
@@ -10,14 +10,14 @@ import java.util.LinkedList;
 public class OhSoOthello extends Agent {
 
     private static final int[][] EVALUATION_MATRIX = {
-        {0, 0, 3, 1, 1, 3, 0, 0},
+        {5, 0, 3, 1, 1, 3, 0, 5},
         {0, 0, 0, 2, 2, 0, 0, 0},
         {3, 0, 1, 1, 1, 1, 0, 3},
         {1, 2, 1, 0, 0, 1, 2, 1},
         {1, 2, 1, 0, 0, 1, 2, 1},
         {3, 0, 1, 1, 1, 1, 0, 3},
         {0, 0, 0, 2, 2, 0, 0, 0},
-        {0, 0, 3, 1, 1, 3, 0, 0}
+        {5, 0, 3, 1, 1, 3, 0, 5}
     };
 
     public OhSoOthello(Color color) {
